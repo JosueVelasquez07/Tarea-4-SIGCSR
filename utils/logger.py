e926113e4a1742aaa21ba78cc645aa9f380f6d52
josue velasuquez <josueingc1@gmail.com>
2026-04-29 16:03:54 -0500

    loggin

--- utils/logger.py
+++ utils/logger.py
@@ -2,14 +2,19 @@ import logging
 
 def configurar_logger():
     logger = logging.getLogger()
+
+    if logger.hasHandlers():
+        return logger
+
     logger.setLevel(logging.INFO)
 
-    handler = logging.FileHandler("sistema_errores.log")
-    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    handler = logging.FileHandler("sistema_errores.log", encoding="utf-8")
+
+    formatter = logging.Formatter(
+        '%(asctime)s - %(levelname)s - %(name)s - %(message)s'
+    )
 
     handler.setFormatter(formatter)
-
-    if not logger.handlers:
-        logger.addHandler(handler)
+    logger.addHandler(handler)
 
     return logger

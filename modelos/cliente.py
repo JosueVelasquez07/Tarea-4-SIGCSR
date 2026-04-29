8c7c5057be51c8b9498b748cee44384821061d38
josue velasuquez <josueingc1@gmail.com>
2026-04-29 09:33:28 -0500

    cambios

--- modelos/cliente.py
+++
@@ -1,8 +0,0 @@
-class cliente:
-    def __init__(self, nome, email, telefone):
-        self.nome = nome
-        self.email = email
-        self.telefone = telefone
-
-    def __str__(self):
-        return f"Cliente: {self.nome}, Email: {self.email}, Telefone: {self.telefone}"

4db9883917b93bc24af0b0cc755654918c23d4e8
josue velasuquez <josueingc1@gmail.com>
2026-04-30 09:08:18 -0500

    actu servicio

--- modelos/servicio.py
+++ modelos/servicio.py
@@ -1,15 +1,25 @@
-from .base import Entidad   
+from abc import abstractmethod
+from .base import Entidad
 
 class Servicio(Entidad):
+
     def __init__(self, id_servicio, nombre, precio):
-        self.id_servicio = id_servicio
-        self.nombre = nombre
-        self.precio = precio
+        self._id_servicio = id_servicio
+        self._nombre = nombre
+        self._precio = precio
 
     def mostrar_info(self):
-        return f"Servicio: {self.nombre}, Precio: {self.precio}"
+        return f"Servicio: {self._nombre}, Precio base: {self._precio}"
 
     def actualizar_info(self, **kwargs):
         for key, value in kwargs.items():
-            if hasattr(self, key):
-                setattr(self, key, value)
+            if hasattr(self, f"_{key}"):
+                setattr(self, f"_{key}", value)
+
+    @abstractmethod
+    def calcular_costo(self, duracion):
+        pass
+
+    # Getter recomendado
+    def get_nombre(self):
+        return self._nombre
